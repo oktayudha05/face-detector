5eb7af2c296d8685d25f73375e7b657ccc7b9e44
oktayudha05 <oktayudha05@gmail.com>
2022-10-01 22:41:58 +0700

    move my file on this repo

---
+++ script.js
@@ -0,0 +1,65 @@
+let video = document.getElementById('video');
+let model;
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext("2d");
+
+const setupCamera = () => {
+    navigator.mediaDevices
+    .getUserMedia({
+        video: {width: 600, height: 400},
+        audio: false,
+    })
+    .then(stream => {
+        video.srcObject = stream;
+    });
+};
+
+const detecFaces = async () => {
+    const prediction = await model.estimateFaces(video, false);
+
+    console.log(prediction);
+
+    ctx.drawImage(video, 0, 0, 600, 400);
+
+    prediction.forEach((pred) => {
+        ctx.beginPath();
+        ctx.lineWidth = '4';
+        ctx.strokeStyle = 'yellow';
+        ctx.rect(
+            pred.topLeft[0],
+            pred.topLeft[1],
+            pred.bottomRight[0] - pred.topLeft[0],
+            pred.bottomRight[1] - pred.topLeft[1]
+        );
+        ctx.stroke();
+
+
+        ctx.fillStyle = 'red';
+        pred.landmarks.forEach((landmark) => {
+            ctx.fileRect(landmark[0], landmark[1], 5, 5);
+        });
+    });
+};
+
+video.addEventListener('loadeddata', async () => {
+    model = await blazeface.load();
+    setInterval(detecFaces, 100);
+}); 
+setupCamera();
+
+
+//*jari detector
+let videoElement = document.getElementById('videoElement');
+const setupVideo = () => {
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia ({video: {width: 600, height: 400}})
+        .then(function(stream) {
+            videoElement.srcObject = stream;
+        })
+        .catch(function(err){
+            console.log(err);
+        });
+    }
+}
+
+setupVideo();
